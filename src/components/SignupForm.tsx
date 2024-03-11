2d435236f292b493bd4703ec8e65f826a87cfc86
AD728 <amritadubey728@gmailcom>
2024-03-11 21:03:43 +0530

    added the signup fields

--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -17,10 +17,16 @@ const SignupForm = () => {
     handleSubmit,
     register,
     formState: { errors },
-  } = useForm<Signup>(); // Specify Signup as the type parameter
+  } = useForm<Signup>();
 
-  const onSubmit: SubmitHandler<Signup> = (data) => {
-    // Use SubmitHandler with Signup type
+  interface Signup {
+    name: string;
+    email: string;
+    password: string;
+    c_password: string;
+  }
+
+  const onSubmit = (data: Signup) => {
     const { name, email, password, c_password } = data;
     localStorage.setItem(
       "cred",
@@ -45,8 +51,89 @@ const SignupForm = () => {
             className="flex flex-col gap-4"
             onSubmit={handleSubmit(onSubmit)}
           >
-            {/* Input fields */}
+            <input
+              className="flex h-10 w-full border border-input text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 rounded-md bg-purple-50 px-4 py-2 text-gray-800 placeholder-gray-400"
+              placeholder="Name"
+              type="text"
+              {...register("name", { required: "Please enter your name" })}
+            />
+            {errors?.name?.message && (
+              <p className="text-red-600 text-sm text-left">
+                {errors?.name?.message?.toString()}
+              </p>
+            )}
+            <input
+              className="flex h-10 w-full border border-input text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 rounded-md bg-purple-50 px-4 py-2 text-gray-800 placeholder-gray-400"
+              placeholder="E-mail"
+              type="email"
+              {...register("email", {
+                required: "Please enter your email",
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: "Invalid email address",
+                },
+              })}
+            />
+            {errors?.email?.message && (
+              <p className="text-red-600 text-sm text-left">
+                {errors?.email?.message?.toString()}
+              </p>
+            )}
+            <input
+              className="flex h-10 w-full border border-input text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 rounded-md bg-purple-50 px-4 py-2 text-gray-800 placeholder-gray-400"
+              placeholder="Enter Password"
+              type="password"
+              {...register("password", {
+                required: "Please enter your password",
+                minLength: {
+                  value: 8,
+                  message: "Password must be at least 8 characters",
+                },
+              })}
+            />
+            {errors?.password?.message && (
+              <p className="text-red-600 text-sm text-left">
+                {errors?.password?.message.toString()}
+              </p>
+            )}
+            <input
+              className="flex h-10 w-full border border-input text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 rounded-md bg-purple-50 px-4 py-2 text-gray-800 placeholder-gray-400"
+              placeholder="Confirm Password"
+              type="password"
+              {...register("c_password", {
+                required: "Please enter your password",
+                validate: (value) =>
+                  value === watch("password") || "Passwords do not match",
+              })}
+            />
+            {errors?.c_password?.message && (
+              <p className="text-red-600 text-sm text-left">
+                {errors?.c_password?.message?.toString()}
+              </p>
+            )}
+            <div className="mt-4 text-xs text-center text-gray-400">
+              By creating an account, you agree to the
+              <Link className="text-orange-700" href="#">
+                Terms and Use
+              </Link>
+              and{"\n  "}
+              <Link className="text-orange-700" href="#">
+                Privacy Policy
+              </Link>
+              .{"\n "}
+            </div>
+            <button
+              type="submit"
+              className="mt-4 rounded-md bg-orange-700 py-2 text-white hover:bg-orange-900"
+            >
+              Sign Up
+            </button>
           </form>
+          <div className="mt-4 text-center text-sm text-gray-400">
+            <Link className="text-orange-700" href="/">
+              I am already a member
+            </Link>
+          </div>
         </div>
       </div>
     </>
